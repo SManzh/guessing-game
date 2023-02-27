e53a80af3a285aa42695848ae9d71762182c9bcc
Slava Manzhela <slavamanzh@gmail.com>
2023-02-27 21:31:11 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        this.mid=Math.round((this.min+this.max)/2);
+        return this.mid;
     }
 
     lower() {
-
+        this.max=this.mid;
     }
 
     greater() {
-
+        this.min=this.mid;
     }
 }
 
